1896773818114fe1b8003be522c0aeb108a250f9
Nasim Miah <nasim@Nasims-MacBook.local>
2025-10-09 00:23:34 +0800

    wpcs fix

--- current-year-by-nasim.php
+++ current-year-by-nasim.php
@@ -24,6 +24,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function cyb_nasim_display_current_year() {
 	$current_year = date_i18n( 'Y' );
-	return esc_html( $current_year );
+	return esc_html( $current_year );// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 }
 add_shortcode( 'cyb_nasim', 'cyb_nasim_display_current_year' );
